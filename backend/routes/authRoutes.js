2726e1814688cb4b3b758d58b3a0cd311ca444d1
Aditya <adityarajsrvofficial@gmail.com>
2025-09-24 22:47:06 +0530

    Made change in authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const axios = require("axios"); 
 
 const router = express.Router();
 
